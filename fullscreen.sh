aa9d59bbca86210b8cce71ac95595dbb1c58d92b
Sze Yick <szeyick@gmail.com>
2017-03-22 22:02:26 +1100

    Update fullscreen.sh
    Added URL to directly load from this script rather than configuration. An issue has been identified where the file loading does not read correctly from the configuration. To simplify it, we just write the URL to this file.

--- fullscreen.sh
+++ fullscreen.sh
@@ -3,23 +3,18 @@
 # This script is responsible for opening the browser
 # and configuring it to be displayed in kiosk mode.
 #
-# It reads the configuration file to load the URL into
-# the browser as soon as the Raspberry Pi loads.
-#
 # The installKiosk.sh will automatically configure the Pi
 # to run this script on startup, however it can be run
 # in isolation to automatically load the browser from the command
 # line.
 #
 # Author: Sze Yick
-# Last Updated: 11/03/2017
+# Last Updated: 22/03/2017
 ##
 
 echo "Opening Epiphany Browser"
-# Loading configuration URL.
-while read -r line; do declare $line; done < configuration
-
 # Opening Browser
-sudo -u pi epiphany-browser -a -i --profile ~/.config $URL --display=:0 &
+# Replace http://www.google.com.au with your own URL
+sudo -u pi epiphany-browser -a --profile ~/.config http://www.google.com.au --display=:0 &
 sleep 30s;
 echo key F11 | xte
